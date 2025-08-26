60fda147b1dbe2e7e3b87bb6128ec7202f89c462
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-08-26 16:09:24 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,8 @@
 def slice_simple():
     texto = "Awesome"
+    print(texto[:3].lower())
+    print(texto[2:5].lower())
+    print(texto[:4].lower()+texto[-3:].lower())
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
