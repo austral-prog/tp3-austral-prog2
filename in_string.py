8aa9f297900202bdfc915521a6748c394dbfcf52
Introducción a la Programación 1 y 2 <mbrunettaalvarezper@mail.austral.edu.ar>
2025-08-26 15:45:48 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,20 @@
 def check_vowels():
+    nombre = input("Ingrese nombre: ")
+    if "a" or "A" in nombre:
+         a = True
+         print(f"Contiene a: {a}")
+    if "e" or "E" in nombre:
+         e = True
+         print(f"Contiene e: {e}")
+    if "i" or "I" in nombre:
+         i = True
+         print(f"Contiene i: {i}")
+    if "o" or "O" in nombre:
+         o = True
+         print(f"Contiene o: {o}")
+    if "u" or "U" in nombre:
+         u = True
+         print(f"Contiene u: {u}")
     # Código a implementar utilizando input.
 
 
